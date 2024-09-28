f0d9406d7e0beebf6e9f993e0a77a5fb8957b969
Ilya Ovramets <72743791+ilya-ovramets@users.noreply.github.com>
2024-09-28 22:51:11 +0300

    Initial project

---
+++ DCT_TEST_ASSIGNMENT/DCT_TEST_ASSIGNMENT/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DCT_TEST_ASSIGNMENT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ DCT_TEST_ASSIGNMENT/DCT_TEST_ASSIGNMENT/ViewModel/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCT_TEST_ASSIGNMENT.ViewModel
+{
+    internal class HomeVM
+    {
+    }
+}
